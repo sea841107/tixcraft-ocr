1ac9811922000e86f642b4e07a8b404603e6f502
dd <pkh_dd@photons.com.tw>
2025-03-11 11:33:55 +0800

    load trained model by onnx

--- ocr.py
+++ ocr.py
@@ -1,36 +1,53 @@
-import torch
+import os
+import numpy as np
 import string
 import captcha_image
-from cnn.normal import CaptchaCNN as CNN
+import onnxruntime as ort
 
 # Constant
-MODEL_PATH = "ocr_model.pth"
+MODEL_PATH = "ocr_v1.0.onnx"
 TEST_IMAGE_PATH = "test.png"
+TEST_IMAGE_FOLDER = "captchas/test"
 IMAGE_SIZE = (120, 100)
 CHARS = string.ascii_lowercase # from 'a' to 'z'
-    
-# Load model
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model = CNN().to(device)
-model.load_state_dict(torch.load(MODEL_PATH, map_location=device, weights_only=True))
-model.eval()
 
 def predict_captcha(image_path):
     # import
     image_array = captcha_image.normalize_image_to_np_array(image_path, IMAGE_SIZE)
     image_array = image_array.reshape(1, 1, IMAGE_SIZE[1], IMAGE_SIZE[0])  # (batch_size, channels, height, width)
+    image_array = image_array.astype(np.float32)
 
     # predict
-    image = torch.tensor(image_array, dtype=torch.float32).to(device)
-    with torch.no_grad():
-        output = model(image)
+    ort_session = ort.InferenceSession(MODEL_PATH)
+    output = ort_session.run(None, {"input.1": image_array})
 
     # decode
-    predicted_indices = output.argmax(dim=2).cpu().numpy().flatten()  # find the target characters (1, 4, 26)
+    predicted_indices = np.argmax(output[0], axis=2).flatten()
     predicted_text = ''.join(CHARS[i] for i in predicted_indices)
 
     return predicted_text
 
+total = 0
+correct = 0
+
 # test
-predicted_text = predict_captcha(TEST_IMAGE_PATH)
-print(f"Result: {predicted_text}")
+for folder in os.listdir(TEST_IMAGE_FOLDER):
+    folder_path = os.path.join(TEST_IMAGE_FOLDER, folder)
+    
+    if os.path.isdir(folder_path):
+        for file_name in os.listdir(folder_path):
+            image_path = os.path.join(folder_path, file_name)
+            predicted_text = predict_captcha(image_path)
+            
+            total += 1
+
+            # folder's name is the answer
+            if predicted_text == folder:
+                correct += 1
+            else:
+                print(f"Wrong | Answer: {folder} / Predict: {predicted_text} / File: {file_name}")
+                
+print(f"Total: {total} / Correct: {correct} / Acc: {correct / total:.4f}")
+
+# predicted_text = predict_captcha(TEST_IMAGE_PATH)
+# print(f"Result: {predicted_text}")
